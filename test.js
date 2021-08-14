12249e2cfd53a9b9ec0294a2988c5e4982d40c71
LiwiaDolinska <liwiamur@gmail.com>
2021-08-14 16:00:34 +0200

    added loops

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-var myAge = 25
+var age = 5
 
-if (myAge < 18 || myAge > 30 || myAge === 25){
+while (age < 10) {
 
-    document.write("You can't come");
-
-} else {
-
-    document.write("You can come!!");
+    console.log("Your age is less than 10")
+    age++;
 }
+
+document.write("You are now over 10");
