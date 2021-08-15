bae43aaf0ed1a1c7eb4cd28d1d4a01e1a84b8af3
LiwiaDolinska <liwiamur@gmail.com>
2021-08-15 11:57:05 +0200

    added constr.functions

--- test.js
+++ test.js
@@ -1,22 +1,38 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
 
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Shaun";
-myCar.drive = function(){ 
-    console.log("now driving"); };
+var myCar = {
 
-myCar.drive();
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+        console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    }
 
-var myCar2 = {
-
-     maxSpeed: 70,
-     driver: "Me",
-     drive: function(speed, time){
-         console.log(speed + time);
-     }
 };
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time);
+};
+  this.logDriver = function(){
+    console.log("driver name is " + this.driver);
+  };
+
+}
+
+var myCar = new Car(70, "Ninja");
+var myCar2 = new Car(40, "Me");
+var myCar3 = new Car(10, "Bond");
+var myCar4 = new Car(90, "Shrek");
+
+myCar.drive(30,5);
+myCar3.logDriver();
+
+
+  
+
