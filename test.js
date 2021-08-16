079f62b2dd180fa12bfc09efdaca9e8337708ca1
LiwiaDolinska <liwiamur@gmail.com>
2021-08-16 18:52:31 +0200

    timer

--- test.js
+++ test.js
@@ -1,31 +1,20 @@
-function setUpEvents(){
+var colourChanger = document.getElementById("colour-changer");
+var colours = ["red", "blue", "green", "pink"];
+var counter = 0;
 
+function changeColour(){
 
-
-   var content = document.getElementById("content");
-   var button = document.getElementById("show-more");
-   
-   button.onclick = function(){
-       if(content.className == "open"){
-           // shrink the box
-           content.className = "";
-           button.innerHTML = "Show More";
-   
-       } else{
-           //expand the box
-           content.className = "open";
-           button.innerHTML = "Show less";
-   
-       }
-   };
-
-
-
+    if (counter >= colours.length){
+        counter = 0;
+    }
+    colourChanger.style.background = colours[counter];
+    counter++;
 }
 
+var myTimer = setInterval(changeColour, 3000);
 
-window.onload = function(){
-  setUpEvents();
- 
+colourChanger.onclick = function(){
+
+    clearInterval(myTimer);
+    colourChanger.innerHTML = "Timer stopped";
 };
-
