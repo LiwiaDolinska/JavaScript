8fa4193036aaa5eb1015c0da7312d7360eca8449
LiwiaDolinska <liwiamur@gmail.com>
2021-08-16 16:58:01 +0200

    poland

--- test.js
+++ test.js
@@ -1,38 +1,12 @@
+alert("hey");
 
-var myCar = {
+var title = document.getElementById("page-title");
+title.onclick = function(){
+  alert("you clicked me");
 
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){
-        console.log(speed * time);
-    },
-    logDriver: function(){
-        console.log("driver name is " + this.driver);
-    }
-
-};
-
-var Car = function(maxSpeed, driver){
-
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time);
-};
-  this.logDriver = function(){
-    console.log("driver name is " + this.driver);
-  };
-
-}
-
-var myCar = new Car(70, "Ninja");
-var myCar2 = new Car(40, "Me");
-var myCar3 = new Car(10, "Bond");
-var myCar4 = new Car(90, "Shrek");
-
-myCar.drive(30,5);
-myCar3.logDriver();
-
-
+};  
+function (){
+  alert("you clicked me");
   
+}
 
